d71f5cd0714cbf37708b6dba00ac8ce4a2aa278f
hailang1485 <hailang1485@126.com>
2014-09-03 21:45:53 +0800

    initial spring mvc

---
+++ src/main/java/com/hailume/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.hailume.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by firefly on 2014/9/3.
+ */
+@Controller
+public class WebController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping(value = "/newpage", method = RequestMethod.GET)
+    public String helloworld(Model model) {
+        model.addAttribute("newvalue", "http://image.qtv.com.cn/0/11/53/57/11535780_842371.jpg");
+        return "newpage";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/newpage.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liming_liu
+  Date: 14-9-1
+  Time: 下午3:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>还撸么？——你们撸吧，我先不撸了</title>
+  </head>
+  <body>
+    <p>有人撸吗？</p>
+    <p>撸起！</p>
+    <p>还撸么？</p>
+    <p>你们撸吧，我先不撸了。</p>
+    <img src="${newvalue}">
+  </body>
+</html>
